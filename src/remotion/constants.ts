8ae4cb2c84aee5b1ca8cd76d26473df0b3630abb
adrian32kkr.github.io <adryanmoldokkr32@gmail.com>
2026-05-24 16:28:44 +0200

    Push clean project code - Love Letter Pro

---
+++ src/remotion/constants.ts
@@ -0,0 +1,5 @@
+export const VIDEO_WIDTH = 1080;
+export const VIDEO_HEIGHT = 1920;
+export const VIDEO_FPS = 30;
+export const DURATION_IN_FRAMES = 15 * VIDEO_FPS;
+export const COLORS = { BG_PRIMARY: '#0a0a0f', ACCENT_ROSE: '#e8457a' };
